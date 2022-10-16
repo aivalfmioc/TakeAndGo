89b2cc8cea2977fd4c783f98673f1787d20c76cb
poppaula <poppaula0106@gmail.com>
2022-10-16 11:14:43 +0300

    'first'

---
+++ src/main/java/com/takeandgo/TakeAndGo/TakeAndGoApplication.java
@@ -0,0 +1,13 @@
+package com.takeandgo.TakeAndGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeAndGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeAndGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takeandgo/TakeAndGo/TakeAndGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takeandgo.TakeAndGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeAndGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
